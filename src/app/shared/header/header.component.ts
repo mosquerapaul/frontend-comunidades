128dbfa9d2150b4326f64e78501e3cca76b8947e
Paul Mosquera <mosquera.paul@gmail.com>
2021-01-05 17:32:49 +0100

    Corregido Menú Collapse

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   navList= ['home', 'dashboard', 'login', 'singup'];
+  isMenuCollapsed: boolean = true;
 
   constructor() { }
 
