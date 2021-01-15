f0f1452d039a2714c7de84b830b5eaa9da822952
Paul Mosquera <mosquera.paul@gmail.com>
2021-01-15 19:24:59 +0100

    Dist version to heroku

---
+++ server.js
@@ -0,0 +1,39 @@
+/******************************************************************
+ *                          EXPRESS ROUTER
+ * Router realizado con Express
+ * Se encargará principalmente de devolver los archivos de audio
+ * solicitados desde el reproductor.
+******************************************************************/
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const normalizePort = require('normalize-port');
+
+const port = normalizePort(process.env.PORT || '3000');
+
+/***********************************************
+ *  ENCODING PARAMETERS
+************************************************/
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+/*          Definiciones
+***************************************/
+
+
+/*         Import de Controladores
+****************************************/
+
+
+/*         Resolución de rutas
+****************************************/
+
+
+/*         Rutas estáticas
+****************************************/
+app.use(['/dist/frontend-comunidades', '/'], express.static('public'));
+app.use('*', (req, res) => res.redirect('/'));
+
+console.log(`Server started at port ${port} look at url: http://localhost:${port}`);
+app.listen(port);
